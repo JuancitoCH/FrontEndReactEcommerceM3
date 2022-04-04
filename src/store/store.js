f8945ccac858b3a9a8c64187205b78f0e98c8f24
JuancitoCH <cjuan.chona@hotmail.com>
2022-04-03 23:51:24 -0300

    base

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import {configureStore} from '@reduxjs/toolkit' 
+// importamos los slice y los usamos como reducers
+
+const store = configureStore({
+    reducer:{
+    }
+})
+
+export default store
